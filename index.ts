ae080f4f212c9a7314a4d372c614f91eb05a164a
dario <darioyzf@gmail.com>
2016-02-07 14:36:01 +0100

    up

---
+++ index.ts
@@ -0,0 +1,176 @@
+import merge = require("json-add");
+import los = require("linux-online-status");
+
+
+interface AudioChannelAnswer {
+    dev: string;
+    active: boolean;
+}
+
+interface AudioAnswer {
+    label: string;
+    dev: string;
+    pulsename: string;
+    active: boolean;
+    channels: AudioChannelAnswer[];
+}
+
+interface VideoChannelAnswer {
+    dev: string;
+    label: string;
+    active: boolean;
+}
+
+interface VideoAnswer {
+    dev: string;
+    label: string;
+    active: boolean;
+    channels: VideoChannelAnswer[];
+    model_id: string;
+    vendor_id: string;
+    resolution: string;
+    bus: string;
+    serial: string;
+}
+
+
+interface ScanAnswer {
+    essid: string;
+    mac: string;
+    signal: string;
+}
+
+interface NetworkAnswer {
+    type: string;
+    mac: string;
+    interface: string;
+    essid?: string;
+    scan?: ScanAnswer[];
+    ip?: string;
+    gateway?: string;
+}
+
+
+
+interface LsusbdevAnswer {
+    dev: string;
+    type: string;
+    hub: string;
+    product: string;
+    id: string;
+}
+
+interface GetDrivesAnswer {
+    filesystem: string;
+    blocks: string;
+    used: string;
+    available: string;
+    capacity: string;
+    mounted: string;
+}
+
+interface IOnlineAnswer {
+    bootId: string;
+    bootTime: number;
+    updatedAt: number;
+    usbDevices: LsusbdevAnswer[];
+    drives: GetDrivesAnswer[];
+    networks: NetworkAnswer[];
+    video: {
+        inputs: VideoAnswer[];
+    };
+    audio: {
+        inputs: AudioAnswer[];
+    };
+}
+
+
+interface IObject {
+    _id: string;
+    updatedAt: string;
+    type: string;
+}
+
+interface IComponents {
+    type: string;
+    uid: string;
+}
+
+
+
+function TimeNow() {
+    return new Date().getTime();
+}
+
+function objectessentials(obj, serial) {
+    if (!obj.updatedAt) {
+        obj.updatedAt = TimeNow();
+    }
+
+    if (!obj.serial) {
+        obj.serial = serial;
+    }
+    return obj;
+}
+
+export = class StoreApi {
+    timezone: string;
+    serial: string;
+    components: IComponents[] = [];
+    system: IOnlineAnswer;
+
+    constructor(components: IComponents[], serial: string, timezone: string) {
+        this.timezone = timezone;
+        this.serial = serial;
+        this.components = components;
+        let sys = this.system;
+        los().then(function(data) {
+            sys = data;
+        }).catch(function(err) {
+            console.log("linux-online-status error:", err);
+        });
+    }
+
+
+    status(obj?: any, type?: string): IObject {
+        let defaultobj = <IObject>{};
+
+        if (obj) {
+            merge(obj, defaultobj);
+            if (type) {
+                obj.type = type;
+            } else {
+                obj.type = "status";
+            }
+        } else {
+            obj = this.system;
+            merge(obj, defaultobj);
+            obj.type = "status";
+        }
+
+        if (!obj._id) {
+            obj._id = obj.type + "_" + this.serial;
+        }
+
+        objectessentials(obj, this.serial);
+
+        return obj;
+    }
+
+
+    data(obj: any, uid: string): IObject {
+
+        objectessentials(obj, this.serial);
+
+        if (!obj._id) {
+            for (let i in this.components) {
+                if (this.components[i].uid === uid) {
+                    obj._id = this.components[i].type + "_" + uid + "_" + obj.updatedAt;
+                }
+            }
+        }
+
+        return obj;
+    }
+}
+
---
+++ test/main.ts
@@ -0,0 +1,6 @@
+import index = require("../index");
+
+let Index=new index([],"o","i");
+
+let data=Index.data({},"")
+let status=Index.status()
