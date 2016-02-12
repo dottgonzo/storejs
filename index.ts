ff78fa01352b7c7cb213dd7a5cc42583810b264d
Dario Caruso <darioyzf@gmail.com>
2016-02-12 21:04:21 +0100

    up

--- index.ts
+++ index.ts
@@ -1,88 +1,4 @@
 import merge = require("json-add");
-import los = require("linux-online-status");
-
-
-interface AudioChannelAnswer {
-    dev: string;
-    active: boolean;
-}
-
-interface AudioAnswer {
-    label: string;
-    dev: string;
-    pulsename: string;
-    active: boolean;
-    channels: AudioChannelAnswer[];
-}
-
-interface VideoChannelAnswer {
-    dev: string;
-    label: string;
-    active: boolean;
-}
-
-interface VideoAnswer {
-    dev: string;
-    label: string;
-    active: boolean;
-    channels: VideoChannelAnswer[];
-    model_id: string;
-    vendor_id: string;
-    resolution: string;
-    bus: string;
-    serial: string;
-}
-
-
-interface ScanAnswer {
-    essid: string;
-    mac: string;
-    signal: string;
-}
-
-interface NetworkAnswer {
-    type: string;
-    mac: string;
-    interface: string;
-    essid?: string;
-    scan?: ScanAnswer[];
-    ip?: string;
-    gateway?: string;
-}
-
-
-
-interface LsusbdevAnswer {
-    dev: string;
-    type: string;
-    hub: string;
-    product: string;
-    id: string;
-}
-
-interface GetDrivesAnswer {
-    filesystem: string;
-    blocks: string;
-    used: string;
-    available: string;
-    capacity: string;
-    mounted: string;
-}
-
-interface IOnlineAnswer {
-    bootId: string;
-    bootTime: number;
-    updatedAt: number;
-    usbDevices: LsusbdevAnswer[];
-    drives: GetDrivesAnswer[];
-    networks: NetworkAnswer[];
-    video: {
-        inputs: VideoAnswer[];
-    };
-    audio: {
-        inputs: AudioAnswer[];
-    };
-}
 
 
 interface IObject {
@@ -90,10 +6,29 @@ interface IObject {
     updatedAt: string;
     type: string;
 }
+enum ETimers {
+    daily,
+    weekly,
+    monthly,
+
+}
 
 interface IComponents {
-    type: string;
+    label: string;
     uid: string;
+    tags: string[];
+    counters?: {
+        tag: string;
+        key: string;
+        timer: ETimers;
+    };
+    sum?: {
+        type: string;
+        key: string;
+        timer: ETimers;
+    };
+    data: Function;
+    create: Function;
 }
 
 
@@ -102,7 +37,7 @@ function TimeNow() {
     return new Date().getTime();
 }
 
-function objectessentials(obj, serial) {
+function objectessentials(obj, serial): {} {
     if (!obj.updatedAt) {
         obj.updatedAt = TimeNow();
     }
@@ -116,12 +51,12 @@ function objectessentials(obj, serial) {
 export = class StoreApi {
     timezone: string;
     serial: string;
-    components: IComponents[] = [];
-    system: IOnlineAnswer;
+    sensors: IComponents[] = [];
 
-    constructor(components: IComponents[], serial: string, timezone: string) {
 
-        if (!components) {
+    constructor(sensors: IComponents[], serial: string, timezone: string) {
+
+        if (!sensors) {
             throw Error("missing object");
         } else if (!serial) {
             throw Error("missing uid");
@@ -134,77 +69,81 @@ export = class StoreApi {
 
 
         this.serial = serial;
-        this.components = components;
-        let sys = this.system;
-        los().then(function(data) {
-            sys = data;
-        }).catch(function(err) {
-            console.log("linux-online-status error:", err);
-        });
-    }
+        this.sensors = sensors;
+        for (var i in this.sensors) {
 
+            this.sensors[i].data = function(tag, obj) {
 
-    status(obj?: any, type?: string): IObject {
-        let defaultobj = <IObject>{};
-
-        if (obj) {
-            merge(obj, defaultobj);
-            if (type) {
-                obj.type = type;
-            } else {
-                obj.type = "status";
-            }
-        } else {
-            obj = this.system;
-            merge(obj, defaultobj);
-            obj.type = "status";
-        }
-
-        if (!obj._id) {
-            obj._id = obj.type + "_" + this.serial;
-        }
-
-        objectessentials(obj, this.serial);
-
-        return obj;
-    }
-
-
-    data(obj: any, uid: string): IObject {
-        if (!obj) {
-            throw Error("missing object");
-        } else if (!uid) {
-            throw Error("missing uid");
-        }
-
-
-        objectessentials(obj, this.serial);
-
-        if (!obj._id) {
-            for (let i in this.components) {
-                if (this.components[i].uid === uid) {
-                    obj._id = this.components[i].type + "_" + uid + "_" + obj.updatedAt;
+                if (!tag) {
+                    throw Error("missing something");
                 }
+
+                objectessentials(obj, this.serial);
+                obj._id = tag + "_" + this.sensors[i].uid + "_" + obj.updatedAt;
+                return obj
+            }
+            this.sensors[i].create = function(tag) {
+                if (!tag) {
+                    throw Error("missing something");
+                }
+                let defaultobj = <IObject>{};
+                objectessentials(defaultobj, this.serial);
+                defaultobj._id = tag + "_" + this.sensors[i].uid + "_" + defaultobj.updatedAt;
+                return defaultobj
             }
         }
-
-        return obj;
-
     }
 
-    new(uid: string) {
-        if (!uid) {
-            throw Error("missing uid");
-        }
+
+
+
+
+    system(obj: any, tag?: string): IObject {
         let defaultobj = <IObject>{};
 
-        objectessentials(defaultobj, this.serial);
-        for (let i in this.components) {
-            if (this.components[i].uid === uid) {
-                defaultobj._id = this.components[i].type + "_" + uid + "_" + defaultobj.updatedAt;
-            }
+        if (obj && tag) {
+            merge(obj, defaultobj);
+
+        } else {
+            throw Error("missed obj or tag")
         }
-        return defaultobj;
+
+        if (!obj._id) {
+            obj._id = tag + "_" + this.serial;
+        }
+
+        objectessentials(obj, this.serial);
+
+        return obj;
     }
+
+
+
+    sensor(uid: string) {
+        
+        let sensor = <IComponents>{};
+        for (let i = 0; i < this.sensors.length; i++) {
+            if (this.sensors[i].uid === uid) {
+
+                sensor = this.sensors[i];
+            }
+
+
+        }
+
+        if (!sensor.uid) {
+            throw Error("no sensor founded")
+        } else {
+
+
+            return sensor
+
+        }
+
+
+
+    }
+
+
 }
 
